8f6e1eb460a494c3d135c278f0394691319ed471
Ali Gamal Ali <aliking90k@gmail.com>
2026-02-05 21:21:41 +0200

    change axios url

--- front/src/api/axiosInstance.js
+++ front/src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie"
 
 const axiosInstance = axios.create({
-    baseURL:"http://localhost:5000",
+    baseURL:"https://conceptual-camala-aligamal-eb60638f.koyeb.app",
     headers: { "Content-Type": "application/json"}
 });
 
